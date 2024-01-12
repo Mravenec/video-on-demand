021c5e053ba6ad0779b8f13685e4620861704df9
Mravenec <Mravenec>
2024-01-12 05:48:07 -0600

    CorsConfig  s1.tuprimernegocio.org

--- database/src/main/java/com/tuprimernegocio/library/database/configuration/CorsConfig.java
+++ database/src/main/java/com/tuprimernegocio/library/database/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://tuprimernegocio.org")
+            .allowedOrigins("https://s1.tuprimernegocio.org")
             .allowedMethods("*") // Permite todos los métodos
             .allowedHeaders("*") // Permite todas las cabeceras
             .allowCredentials(true)
